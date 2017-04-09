e2bd8f4f3b2c35984b4069e415213bf8d6b80fc5
talta <t.taltallc@gmail.com>
2017-04-08 21:17:43 -0400

    mongoDB to point to mLab

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 exports.DATABASE_URL = (process.env.DATABASE_URL ||
 						global.DATABASE_URL ||
-						'mongodb://localhost/quiz-engine'
+						'mongodb://tatest:Password1234@ds149278.mlab.com:49278/quiz-engine'
 						);
 
 exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL ||
